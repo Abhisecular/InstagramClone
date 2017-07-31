7f634594ea318b34f85582c0090a5d4920ca06e0
Abhisecular <secular.abhishek1996@gmail.com>
2017-08-01 01:00:28 +0530

    Commit.....

--- app/src/main/java/com/example/abhi/instagramclone/Profile/AccountSettingsActivity.java
+++ app/src/main/java/com/example/abhi/instagramclone/Profile/AccountSettingsActivity.java
@@ -2,13 +2,17 @@ package com.example.abhi.instagramclone.Profile;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import com.example.abhi.instagramclone.R;
+import com.example.abhi.instagramclone.Utils.SectionsStatePagerAdapter;
 
 import java.util.ArrayList;
 
@@ -17,12 +21,18 @@ import java.util.ArrayList;
  */
 
 public class AccountSettingsActivity extends AppCompatActivity {
+
+    private SectionsStatePagerAdapter pagerAdapter;
+    private ViewPager mViewPager;
+    private RelativeLayout mRelativeLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accountsettings);
+        mViewPager = (ViewPager)findViewById(R.id.container);
+        mRelativeLayout = (RelativeLayout)findViewById(R.id.relLayout1);
         setupSettingsList();
-
+        setupFragments();
         ImageView imageView = (ImageView)findViewById(R.id.backArrow);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,14 +42,32 @@ public class AccountSettingsActivity extends AppCompatActivity {
         });
     }
 
+    private void setupFragments(){
+        pagerAdapter = new SectionsStatePagerAdapter(getSupportFragmentManager()); 
+        pagerAdapter.addFragment(new EditProfileFragment(), getString(R.string.edit_profile_fragment));
+        pagerAdapter.addFragment(new SignOutFragment(), getString(R.string.sign_out_fragment));
+
+    }
+
+    private void setViewPager(int fragmentNumber){
+        mRelativeLayout.setVisibility(View.GONE);
+        mViewPager.setAdapter(pagerAdapter);
+        mViewPager.setCurrentItem(fragmentNumber);
+    }
+
     private void setupSettingsList(){
         ListView listView= (ListView)findViewById(R.id.lvAccountSettings);
         ArrayList<String> options = new ArrayList<>();
-        options.add(getString(R.string.edit_profile));
-        options.add(getString(R.string.sign_out));
+        options.add(getString(R.string.edit_profile_fragment));
+        options.add(getString(R.string.sign_out_fragment));
         ArrayAdapter adapter = new ArrayAdapter(AccountSettingsActivity.this, android.R.layout.simple_list_item_1, options);
         listView.setAdapter(adapter);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                setViewPager(position);
+            }
+        });
 
     }
 }
